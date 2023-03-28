06470bce5bac100d3df9d7471222cca54452b2d8
richardsoriano <soriano.richard2020@gmail.com>
2023-03-27 19:51:08 -0700

    Initialize

---
+++ vidly2/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using vidly2.Models;
+using vidly2.ViewModels;
+
+namespace vidly2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "The Mandelorian" },
+                new Customer { Name = "Captain Kirk" }
+
+            };
+            var viewModel = new RandomMovieViewModel
+          
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+       
+        }
+        public IActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+       public IActionResult ByReleaseDate(int year, byte month) 
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ vidly2/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,12 @@
+﻿using vidly2.Models;
+
+namespace vidly2.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+  
+        public Movie Movie { get; set; }
+        public List<Customer>    Customers { get; set; }
+    }
+
+}
